4e52161a35218020a9b1b45d21dce05ebf475b9f
paulc@dogwood-minor.local <paulc@dogwood-minor.local>
2010-09-19 16:03:47 +0100

    Add tests to DNSRecord / Create README from __doc__

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import Command,setup
+
+import src.dns
+long_description = src.dns.DNSRecord.__doc__.rstrip() + "\n"
+
+class GenerateReadme(Command):
+    description = "Generates README file from long_description"
+    user_options = []
+    def initialize_options(self): pass
+    def finalize_options(self): pass
+    def run(self):
+        open("README","w").write(long_description)
 
 setup(name='dnslib',
       version = '0.1',
       description = 'Simple library to encode/decode DNS wire-format packets',
-      long_description = open("src/README").read(),
+      long_description = long_description,
       author = 'Paul Chakravarti',
       author_email = 'paul.chakravarti@gmail.com',
       url = 'http://bitbucket.org/paulc/dnslib/',
+      cmdclass = { 'readme' : GenerateReadme },
       packages = ['dnslib'],
       package_dir = {'dnslib':'src'},
       license = 'BSD'
