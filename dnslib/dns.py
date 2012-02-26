895fefdefa9f3acda89b973a71433316840ffdd0
Paul Chakravarti <paul.chakravarti@gmail.com>
2012-02-26 22:45:00 +0000

    Merge with dnslib-circuits

--- dnslib/dns.py
+++ dnslib/dns.py
@@ -7,7 +7,7 @@ from label import DNSLabel,DNSLabelError,DNSBuffer
 
 QTYPE =  Bimap({ 1:'A', 2:'NS', 3:'MD', 4:'MF', 5:'CNAME', 6:'SOA', 7:'MB', 
                  8:'MG', 9:'MR', 10:'NULL', 11:'WKS', 12:'PTR', 13:'HINFO',
-                 14:'MINFO', 15:'MX', 16:'TXT',26:'AAAA',252:'AXFR',253:'MAILB',
+                 14:'MINFO', 15:'MX', 16:'TXT',28:'AAAA',252:'AXFR',253:'MAILB',
                  254:'MAILA',255:'*'})
 CLASS =  Bimap({ 1:'IN', 2:'CS', 3:'CH', 4:'Hesiod', 255:'*'})
 QR =     Bimap({ 0:'QUERY', 1:'RESPONSE' })
