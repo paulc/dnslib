7ef1b52f7803518b4022b36f3789cc33a64d7294
Paul Chakravarti <paul.chakravarti@gmail.com>
2012-02-26 23:04:46 +0000

    Update DNS record types

--- dnslib/dns.py
+++ dnslib/dns.py
@@ -5,9 +5,14 @@ from bit import get_bits,set_bits
 from bimap import Bimap
 from label import DNSLabel,DNSLabelError,DNSBuffer
 
-QTYPE =  Bimap({ 1:'A', 2:'NS', 5:'CNAME', 6:'SOA', 12:'PTR', 
-                 13:'HINFO', 15:'MX', 16:'TXT',28:'AAAA',
-                 252:'AXFR', 255:'*'})
+QTYPE =  Bimap({1:'A', 2:'NS', 5:'CNAME', 6:'SOA', 12:'PTR', 15:'MX',
+                16:'TXT', 17:'RP', 18:'AFSDB', 24:'SIG', 25:'KEY',
+                28:'AAAA', 29:'LOC', 33:'SRV', 35:'NAPTR', 36:'KX',
+                37:'CERT', 39:'DNAME', 41:'OPT', 42:'APL', 43:'DS',
+                44:'SSHFP', 45:'IPSECKEY', 46:'RRSIG', 47:'NSEC',
+                48:'DNSKEY', 49:'DHCID', 50:'NSEC3', 51:'NSEC3PARAM',
+                55:'HIP', 99:'SPF', 249:'TKEY', 250:'TSIG', 251:'IXFR',
+                252:'AXFR', 255:'*', 32768:'TA', 32769:'DLV'})
 CLASS =  Bimap({ 1:'IN', 2:'CS', 3:'CH', 4:'Hesiod', 255:'*'})
 QR =     Bimap({ 0:'QUERY', 1:'RESPONSE' })
 RCODE =  Bimap({ 0:'None', 1:'Format Error', 2:'Server failure', 
@@ -124,7 +129,7 @@ class DNSRecord(object):
 
     """
 
-    version = "0.4.0"
+    version = "0.4.1"
 
     @classmethod
     def parse(cls,packet):
