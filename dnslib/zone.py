532b6ac45cd61be71bd6e8ad3606d2d15e95abc6
Paul Chakravarti <paul.chakravarti@gmail.com>
2014-02-15 09:00:40 +0000

    Clean up zone.py
    --HG--
    branch : py3

--- dnslib/zone.py
+++ dnslib/zone.py
@@ -12,121 +12,120 @@ def parse_time(s):
     else:
         return int(s)
 
-def zone_iter(zone):
+class ZoneParser:
 
-    l = WordLexer(zone)
-    l.commentchars = ';'
-    l.nltok = ('NL',)
-    l.spacetok = ('SPACE',)
-    i = iter(l)
+    """
+        Zone file parser
 
-    rr = []
-    prev = None
-    paren = False
-    prev_label = DNSLabel(".")
-    origin = DNSLabel(".")
-    ttl = 0
+        >>> z = ZoneParser(zone)
+        >>> for rr in z:
+        ...     print(rr)
+        ('example.com', 5400, 'IN', 'SOA', ['ns1.example.com.', 'admin.example.com.', '2014020901', '10800', '1800', '604800', '86400'])
+        ('example.com', 1800, 'IN', 'NS', ['ns1.example.com.'])
+        ('example.com', 5400, 'IN', 'MX', ['10', 'mail.example.com.'])
+        ('abc.example.com', 5400, 'IN', 'A', ['1.2.3.4'])
+        ('abc.example.com', 5400, 'IN', 'TXT', ['A B C'])
+        ('ns1.example.com', 60, 'IN', 'A', ['6.7.8.9'])
+        ('ipv6.example.com', 5400, 'IN', 'AAAA', ['1234:5678::1'])
+        ('www.example.com', 5400, 'IN', 'CNAME', ['abc'])
+        ('4.3.2.1.5.5.5.0.0.8.1.e164.arpa', 300, 'IN', 'NAPTR', ['100', '10', 'U', 'E2U+sip', '!^.*$!sip:customer-service@example.com!', '.'])
+    """
 
-    def parse_label(label):
-        if label.endswith("."):
-            return DNSLabel(label)
-        elif label == "@":
-            return origin
-        elif label == '':
-            return prev_label
+    def __init__(self,zone,origin="",ttl=0):
+        self.l = WordLexer(zone)
+        self.l.commentchars = ';'
+        self.l.nltok = ('NL',)
+        self.l.spacetok = ('SPACE',)
+        self.i = iter(self.l)
+        if type(origin) is DNSLabel:
+            self.origin = origin
         else:
-            return origin.add(label)
+            self.origin= DNSLabel(origin)
+        self.label = DNSLabel("")
+        self.prev = None
 
-    for tok in i:
-        if tok[0] == 'NL':
-            if not paren and rr:
-                _label = prev_label = parse_label(rr.pop(0))
-                if rr[0].isdigit():
-                    _ttl = int(rr.pop(0))
+    def parse_label(self,label):
+        if label.endswith("."):
+            self.label = DNSLabel(label)
+        elif label == "@":
+            self.label = self.origin
+        elif label == '':
+            pass
+        else:
+            self.label = self.origin.add(label)
+        return self.label
+
+    def parse_rr(self,rr):
+        label = self.parse_label(rr.pop(0))
+        ttl = int(rr.pop(0)) if rr[0].isdigit() else self.ttl
+        cl = rr.pop(0) if rr[0] in ('IN','CH','HS') else 'IN'
+        rtype = rr.pop(0)
+        rdata = rr
+        return((label,ttl,cl,rtype,rdata))
+
+    def __iter__(self):
+        return self.parse()
+
+    def parse(self):
+        while True:
+            yield self.next()
+
+    def next(self):
+        rr = []
+        paren = False
+        while True:
+            tok = next(self.i)
+            if tok[0] == 'NL':
+                if not paren and rr:
+                    self.prev = tok[0]
+                    return self.parse_rr(rr)
+            elif tok[0] == 'SPACE' and self.prev == 'NL' and not paren:
+                rr.append('')
+            elif tok[0] == 'ATOM':
+                if tok[1] == '(':
+                    paren = True
+                elif tok[1] == ')':
+                    paren = False
+                elif tok[1] == '$ORIGIN':
+                    _ = next(self.i) # Skip space
+                    self.origin = self.label = DNSLabel(next(self.i)[1])
+                elif tok[1] == '$TTL':
+                    _ = next(self.i) # Skip space
+                    self.ttl = parse_time(next(self.i)[1])
                 else:
-                    _ttl = ttl
-                if rr[0] in ('IN'):
-                    _class = rr.pop(0)
-                else:
-                    _class = 'IN'
-                _type = rr.pop(0)
-                _rdata = rr
-                yield((_label,_ttl,_class,_type,_rdata))
-                rr = []
-        elif tok[0] == 'SPACE' and prev[0] == 'NL' and not paren:
-            rr.append('')
-        elif tok[0] == 'ATOM':
-            if tok[1] == '(':
-                paren = True
-            elif tok[1] == ')':
-                paren = False
-            elif tok[1] == '$ORIGIN':
-                next(i) # Skip space
-                origin = prev_label = DNSLabel(next(i)[1])
-            elif tok[1] == '$TTL':
-                next(i) # Skip space
-                ttl = parse_time(next(i)[1])
-            else:
-                rr.append(tok[1])
-        prev = tok
+                    rr.append(tok[1])
+            self.prev = tok[0]
 
+if __name__ == '__main__':
 
-zone1 = """
-$ORIGIN example.com.        ; Comment
-$TTL 90m
+    import doctest,textwrap
+    zone = textwrap.dedent("""
+        $ORIGIN example.com.        ; Comment
+        $TTL 90m
 
-@       IN  SOA     ns1.example.com. admin.example.com. (
-                        2014011500  ; Serial
-                        12h         ; Stuff
-                        15m 
-                        3w 
-                        3h 
-                    )
-        86400 IN  NS      ns1.example.com.
-xxx        IN  NS      ns1.example.com.
+        @           IN  SOA     ns1.example.com. admin.example.com. (
+                                    2014020901  ; Serial
+                                    10800   ; Refresh
+                                    1800    ; Retry
+                                    604800  ; Expire
+                                    86400 ) ; Minimum TTL
 
-        IN  MX      ( 10  mail.example.com. )
-        IN  A       1.2.3.4
-        IN  TXT     ( "A  B  C" )
-abc  60   IN  A       6.7.8.9
-        IN  TXT     "Stuff"
-ipv6    IN  AAAA    1234:5678::1
-www     IN  CNAME   abc
-$TTL    5m
-xxx.yyy.com.    IN  A   9.9.9.9
-                IN  TXT "Some     
-                        Text"
-last    40  IN  HINFO   "HW Info" "SW"
-        IN A 9.9.9.9
+             1800   IN  NS      ns1.example.com.
+                    IN  MX      ( 10  mail.example.com. )
 
-$TTL 5s
-$ORIGIN 4.3.2.1.5.5.5.0.0.8.1.e164.arpa.
-    IN NAPTR ( 100 10 "U" "E2U+sip" "!^.*$!sip:customer-service@example.com!" . )
-    IN NAPTR ( 102 10 "U" "E2U+email" "!^.*$!mailto:information@example.com!" . )
+        abc         IN  A       1.2.3.4
+                    IN  TXT     "A B C"
 
-"""
+        ns1   60    IN  A       6.7.8.9
+        ipv6        IN  AAAA    1234:5678::1
+        www         IN  CNAME   abc
 
-zone2 = """
-ipv6.pchak.net. 86400   IN  SOA ns1.he.net. hostmaster.he.net. (
-                    2014020901  ; Serial
-                    10800   ; Refresh
-                    1800    ; Retry
-                    604800  ; Expire
-                    86400 ) ; Minimum TTL
-ipv6.pchak.net. 3600    IN  NS  ns2.he.net.
-ipv6.pchak.net. 3600    IN  NS  ns3.he.net.
-ipv6.pchak.net. 3600    IN  NS  ns4.he.net.
-ipv6.pchak.net. 3600    IN  NS  ns5.he.net.
-home.ipv6.pchak.net.    3600    IN  AAAA    2001:470:6d:33:95c0:2178:58d1:6803
-vds6.ipv6.pchak.net.    3600    IN  AAAA    2a01:4f8:150:1102:0:0:0:fa
-ipv6.pchak.net. 3600    IN  AAAA    2001:41d0:a:105f:0:0:0:1
-"""
+        $TTL 5m
+        $ORIGIN 4.3.2.1.5.5.5.0.0.8.1.e164.arpa.
+                    IN  NAPTR   ( 100 10 "U" "E2U+sip" 
+                                  "!^.*$!sip:customer-service@example.com!" 
+                                  . )
+    """)
 
-for rr in zone_iter(zone1):
-    print(rr)
-
-print()
-
-for rr in zone_iter(zone2):
-    print(rr)
+    doctest.testmod()
 
