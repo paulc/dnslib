ce20f19d815e3313224f734469ee16438acd1f3a
Paul Chakravarti <paul.chakravarti@gmail.com>
2014-02-20 14:21:49 +0000

    Modify Bimap to take custom exception
    --HG--
    branch : py3

--- dnslib/bimap.py
+++ dnslib/bimap.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from dnslib.error import DNSError
-
 class BimapError(Exception):
     pass
 
@@ -11,16 +9,28 @@ class Bimap(object):
 
     Automatic bi-directional mapping between value and text. 
 
-        >>> TEST = Bimap('TEST',{1:'A', 2:'B', 3:'C'})
+        >>> class TestError(Exception):
+        ...     pass
+
+        >>> TEST = Bimap('TEST',{1:'A', 2:'B', 3:'C'},TestError)
         >>> TEST[1]
         'A'
         >>> TEST.A
         1
+        >>> TEST.X
+        Traceback (most recent call last):
+        ...
+        TestError: TEST: Invalid reverse lookup: [X]
+        >>> TEST[99]
+        Traceback (most recent call last):
+        ...
+        TestError: TEST: Invalid forward lookup: [99]
     
     """
 
-    def __init__(self,name,forward):
+    def __init__(self,name,forward,error=KeyError):
         self.name = name
+        self.error = error
         self.forward = forward.copy()
         self.reverse = dict([(v,k) for (k,v) in list(forward.items())])
 
@@ -28,13 +38,13 @@ class Bimap(object):
         try:
             return self.forward[k]
         except KeyError as e:
-            raise DNSError("%s: Invalid forward lookup: [%s]" % (self.name,k))
+            raise self.error("%s: Invalid forward lookup: [%s]" % (self.name,k))
 
     def __getattr__(self,k):
         try:
             return self.reverse[k]
         except KeyError as e:
-            raise DNSError("%s: Invalid reverse lookup: [%s]" % (self.name,k))
+            raise self.error("%s: Invalid reverse lookup: [%s]" % (self.name,k))
 
 if __name__ == '__main__':
     import doctest
--- dnslib/dns.py
+++ dnslib/dns.py
@@ -12,20 +12,31 @@ from dnslib.label import DNSLabel,DNSLabelError,DNSBuffer
 from dnslib.zone import ZoneParser
 from dnslib.error import DNSError
 
-QTYPE =  Bimap('QTYPE', {1:'A', 2:'NS', 5:'CNAME', 6:'SOA', 12:'PTR', 15:'MX',
-                16:'TXT', 17:'RP', 18:'AFSDB', 24:'SIG', 25:'KEY', 28:'AAAA',
-                29:'LOC', 33:'SRV', 35:'NAPTR', 36:'KX', 37:'CERT', 39:'DNAME',
-                41:'OPT', 42:'APL', 43:'DS', 44:'SSHFP', 45:'IPSECKEY',
-                46:'RRSIG', 47:'NSEC', 48:'DNSKEY', 49:'DHCID', 50:'NSEC3',
-                51:'NSEC3PARAM', 55:'HIP', 99:'SPF', 249:'TKEY', 250:'TSIG',
-                251:'IXFR', 252:'AXFR', 255:'ANY', 32768:'TA', 32769:'DLV'})
+class DNSError(Exception):
+    pass
 
-CLASS =  Bimap('CLASS',{1:'IN', 2:'CS', 3:'CH', 4:'Hesiod', 254:'None', 255:'*'})
-QR =     Bimap('QR',{0:'QUERY', 1:'RESPONSE'})
-RCODE =  Bimap('RCODE',{0:'NOERROR', 1:'FORMERR', 2:'SERVFAIL', 
-                 3:'NXDOMAIN', 4:'NOTIMP', 5:'REFUSED', 6:'YXDOMAIN',
-                 7:'YXRRSET', 8:'NXRRSET', 9:'NOTAUTH', 10:'NOTZONE'})
-OPCODE = Bimap('OPCODE',{0:'QUERY', 1:'IQUERY', 2:'STATUS', 5:'UPDATE'})
+QTYPE =  Bimap('QTYPE', 
+                {1:'A', 2:'NS', 5:'CNAME', 6:'SOA', 12:'PTR', 15:'MX',
+                 16:'TXT', 17:'RP', 18:'AFSDB', 24:'SIG', 25:'KEY', 28:'AAAA',
+                 29:'LOC', 33:'SRV', 35:'NAPTR', 36:'KX', 37:'CERT', 39:'DNAME',
+                 41:'OPT', 42:'APL', 43:'DS', 44:'SSHFP', 45:'IPSECKEY',
+                 46:'RRSIG', 47:'NSEC', 48:'DNSKEY', 49:'DHCID', 50:'NSEC3',
+                 51:'NSEC3PARAM', 55:'HIP', 99:'SPF', 249:'TKEY', 250:'TSIG',
+                 251:'IXFR', 252:'AXFR', 255:'ANY', 32768:'TA', 32769:'DLV'},
+                DNSError)
+CLASS =  Bimap('CLASS',
+                {1:'IN', 2:'CS', 3:'CH', 4:'Hesiod', 254:'None', 255:'*'},
+                DNSError)
+QR =     Bimap('QR',
+                {0:'QUERY', 1:'RESPONSE'},
+                DNSError)
+RCODE =  Bimap('RCODE',
+                {0:'NOERROR', 1:'FORMERR', 2:'SERVFAIL', 3:'NXDOMAIN', 
+                 4:'NOTIMP', 5:'REFUSED', 6:'YXDOMAIN', 7:'YXRRSET', 
+                 8:'NXRRSET', 9:'NOTAUTH', 10:'NOTZONE'},
+                DNSError)
+OPCODE = Bimap('OPCODE',{0:'QUERY', 1:'IQUERY', 2:'STATUS', 5:'UPDATE'},
+                DNSError)
 
 def append_flat(a,b):
     a.extend(b) if (type(b) in (tuple,list)) else a.append(b)
