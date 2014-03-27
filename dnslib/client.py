15eb329f8af16a6fd321e506900a4ce6301e3d1b
Paul Chakravarti <paul.chakravarti@gmail.com>
2014-03-27 21:09:02 +0000

    Add --diff option
    --HG--
    branch : py3

--- dnslib/client.py
+++ dnslib/client.py
@@ -18,10 +18,7 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser(description="DNS Client")
     p.add_argument("--address","-a",default="8.8.8.8",
                     metavar="<address>",
-                    help="Server address (default:8.8.8.8)")
-    p.add_argument("--port","-p",type=int,default=53,
-                    metavar="<port>",
-                    help="Server port (default:53)")
+                    help="Server address[:port] (default:8.8.8.8:53)")
     p.add_argument("--query",action='store_true',default=False,
                     help="Show query (default: False)")
     p.add_argument("--hex",action='store_true',default=False,
@@ -30,8 +27,10 @@ if __name__ == '__main__':
                     help="Use TCP (default: UDP)")
     p.add_argument("--noretry",action='store_true',default=False,
                     help="Don't retry query using TCP if truncated (default: false)")
+    p.add_argument("--diff",default="",
+            help="Compare response from alternate nameserver (format: address:port / default: false)")
     p.add_argument("--dig",action='store_true',default=False,
-                    help="Run query with DiG and compare result (default: false)")
+                    help="Compare result with DiG (default: false)")
     p.add_argument("--debug",action='store_true',default=False,
                     help="Drop into CLI after request (default: false)")
     p.add_argument("domain",metavar="<domain>",
@@ -42,6 +41,9 @@ if __name__ == '__main__':
 
     q = DNSRecord(q=DNSQuestion(args.domain,getattr(QTYPE,args.qtype)))
 
+    address,_,port = args.address.partition(':')
+    port = int(port or 53)
+
     if args.query:
         print(";; Sending%s:" % (" (TCP)" if args.tcp else ""))
         if args.hex:
@@ -49,12 +51,12 @@ if __name__ == '__main__':
         print(q)
         print()
 
-    a_pkt = q.send(args.address,args.port,tcp=args.tcp)
+    a_pkt = q.send(address,port,tcp=args.tcp)
     a = DNSRecord.parse(a_pkt)
 
     if a.header.tc and args.noretry == False:
         print(";; Truncated - trying TCP:")
-        a_pkt = q.send(args.address,args.port,tcp=True)
+        a_pkt = q.send(address,port,tcp=True)
         a = DNSRecord.parse(a_pkt)
 
     print(";; Got answer:")
@@ -63,19 +65,34 @@ if __name__ == '__main__':
     print(a)
     print()
 
-    if args.dig:
-        dig = getoutput("dig +qr -p %d %s %s @%s" % (
-                            args.port, args.domain, args.qtype, args.address))
-        dig_reply = list(iter(DigParser(dig)))
-        # DiG might have retried in TCP mode so get last q/a
-        q_dig = dig_reply[-2]
-        a_dig = dig_reply[-1]
-        if q != q_dig:
-            print(";;; ERROR: DiG Question differs")
-            pprint.pprint(q.diff(q_dig))
-        if a != a_dig:
-            print(";;; ERROR: DiG Response differs")
-            pprint.pprint(a.diff(a_dig))
+    if args.dig or args.diff:
+        if args.diff:
+            address,_,port = args.diff.partition(':')
+            port = int(port or 53)
+
+        if args.dig:
+            dig = getoutput("dig +qr -p %d %s %s @%s" % (
+                                port, args.domain, args.qtype, address))
+            dig_reply = list(iter(DigParser(dig)))
+            # DiG might have retried in TCP mode so get last q/a
+            q_diff = dig_reply[-2]
+            a_diff = dig_reply[-1]
+        else:
+            q_diff = DNSRecord(q=DNSQuestion(args.domain,
+                                             getattr(QTYPE,args.qtype)))
+            diff = q_diff.send(address,port,tcp=args.tcp)
+            a_diff = DNSRecord.parse(diff)
+
+            if a_diff.header.tc and args.noretry == False:
+                diff = q_diff.send(address,port,tcp=True)
+                a_diff = DNSRecord.parse(diff)
+
+        if q != q_diff:
+            print(";;; ERROR: Diff Question differs")
+            pprint.pprint(q.diff(q_diff))
+        if a != a_diff:
+            print(";;; ERROR: Diff Response differs")
+            pprint.pprint(a.diff(a_diff))
 
     if args.debug:
         code.interact(local=locals())
