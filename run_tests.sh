164cb01336e362d3074271ac5993ae94dcdbea59
Paul Chakravarti <paul.chakravarti@gmail.com>
2014-08-24 08:03:22 +0100

    Add digparser.py

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=$(pwd)
 
 : ${VERSIONS:="python python3"}
 
-for src in __init__.py bimap.py bit.py buffer.py label.py dns.py lex.py server.py test_decode.py
+for src in __init__.py bimap.py bit.py buffer.py label.py dns.py lex.py server.py digparser.py test_decode.py
 do
 	echo "===" $src
 	for py in $VERSIONS
