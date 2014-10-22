6252002b2b0dcdaeaaa4e574b404849620476602
Paul Chakravarti <paul.chakravarti@gmail.com>
2014-10-22 22:51:38 +0100

    Add EDNS0 class to make it easier to create EDNS0 pseudo-records

--- dnslib/ranges.py
+++ dnslib/ranges.py
@@ -12,6 +12,7 @@
     ...     b = B('b')
     ...     c = H('c')
     ...     d = I('d')
+    ...     e = instance_property('e',(int,bool))
     >>> t = T()
     >>> for i in [0,100,-100]:
     ...     t.a = i
@@ -28,14 +29,61 @@
     Traceback (most recent call last):
     ...
     ValueError: Attribute 'a' must be between -100-100 [blah]
+    >>> t.e = 999
+    >>> t.e = False
+    >>> t.e = None
+    Traceback (most recent call last):
+    ...
+    ValueError: Attribute 'e' must be instance of ...
+
+    >>> check_range("test",123,0,255)
+    >>> check_range("test",999,0,255)
+    Traceback (most recent call last):
+    ...
+    ValueError: Attribute 'test' must be between 0-255 [999]
+
+    >>> check_instance("test",123,int)
+    >>> check_instance("test","xxx",int)
+    Traceback (most recent call last):
+    ...
+    ValueError: Attribute 'test' must be instance of ...
 
 """
 
 import sys
 if sys.version < '3':
     int_types = (int, long,)
+    byte_types = (str,bytearray)
 else:
     int_types = (int,)
+    byte_types = (bytes,bytearray)
+
+def check_instance(name,val,types):
+    if not isinstance(val,types):
+        raise ValueError("Attribute '%s' must be instance of %s [%s]" % 
+                                        (name,types,type(val)))
+
+def check_bytes(name,val):
+    return check_instance(name,val,byte_types)
+
+def instance_property(attr,types):
+    def getter(obj):
+        return getattr(obj,"_%s" % attr)
+    def setter(obj,val):
+        if isinstance(val,types):
+            setattr(obj,"_%s" % attr,val)
+        else:
+            raise ValueError("Attribute '%s' must be instance of %s [%s]" % 
+                                        (attr,types,type(val)))
+    return property(getter,setter)
+
+def BYTES(attr):
+    return instance_property(attr,byte_types)
+
+def check_range(name,val,min,max):
+    if not (isinstance(val,int_types) and min <= val <= max):
+        raise ValueError("Attribute '%s' must be between %d-%d [%s]" % 
+                                        (name,min,max,val))
 
 def range_property(attr,min,max):
     def getter(obj):
@@ -89,5 +137,5 @@ def IP6(attr):
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
 
