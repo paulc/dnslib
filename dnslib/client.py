9b5ac42048980098cd3e8f15344eab2e45f496d5
PaulC <none@none>
2019-03-24 11:13:10 +0000

    Catch DNSError in dnslib.client

--- dnslib/client.py
+++ dnslib/client.py
@@ -18,9 +18,9 @@ try:
 except ImportError:
     from commands import getoutput
 
-import binascii,code,pprint
+import binascii,code,pprint,sys
 
-from dnslib.dns import DNSRecord,DNSHeader,DNSQuestion,QTYPE,EDNS0
+from dnslib.dns import DNSRecord,DNSHeader,DNSQuestion,DNSError,QTYPE,EDNS0
 from dnslib.digparser import DigParser
 
 if __name__ == '__main__':
@@ -56,84 +56,88 @@ if __name__ == '__main__':
     args = p.parse_args()
 
     # Construct request
-    q = DNSRecord(q=DNSQuestion(args.domain,getattr(QTYPE,args.qtype)))
+    try:
+        q = DNSRecord(q=DNSQuestion(args.domain,getattr(QTYPE,args.qtype)))
 
-    if args.dnssec:
-        q.add_ar(EDNS0(flags="do",udp_len=4096))
-        q.header.ad = 1
+        if args.dnssec:
+            q.add_ar(EDNS0(flags="do",udp_len=4096))
+            q.header.ad = 1
 
-    address,_,port = args.server.partition(':')
-    port = int(port or 53)
+        address,_,port = args.server.partition(':')
+        port = int(port or 53)
 
-    if args.query:
-        print(";; Sending%s:" % (" (TCP)" if args.tcp else ""))
-        if args.hex:
-            print(";; QUERY:",binascii.hexlify(q.pack()).decode())
-        print(q)
-        print()
+        if args.query:
+            print(";; Sending%s:" % (" (TCP)" if args.tcp else ""))
+            if args.hex:
+                print(";; QUERY:",binascii.hexlify(q.pack()).decode())
+            print(q)
+            print()
 
-    a_pkt = q.send(address,port,tcp=args.tcp)
-    a = DNSRecord.parse(a_pkt)
-
-    if a.header.tc and args.noretry == False:
-        # Truncated - retry in TCP mode
-        a_pkt = q.send(address,port,tcp=True)
+        a_pkt = q.send(address,port,tcp=args.tcp)
         a = DNSRecord.parse(a_pkt)
 
-    if args.dig or args.diff:
-        if args.diff:
-            address,_,port = args.diff.partition(':')
-            port = int(port or 53)
-
-        if args.dig:
-            if args.dnssec:
-                dig = getoutput("dig +qr +dnssec -p %d %s %s @%s" % (
-                                port, args.domain, args.qtype, address))
-            else:
-                dig = getoutput("dig +qr +noedns +noadflag -p %d %s %s @%s" % (
-                                port, args.domain, args.qtype, address))
-            dig_reply = list(iter(DigParser(dig)))
-            # DiG might have retried in TCP mode so get last q/a
-            q_diff = dig_reply[-2]
-            a_diff = dig_reply[-1]
-        else:
-            q_diff = DNSRecord(header=DNSHeader(id=q.header.id),
-                               q=DNSQuestion(args.domain,
-                                             getattr(QTYPE,args.qtype)))
-            q_diff = q
-            diff = q_diff.send(address,port,tcp=args.tcp)
-            a_diff = DNSRecord.parse(diff)
-            if a_diff.header.tc and args.noretry == False:
-                diff = q_diff.send(address,port,tcp=True)
-                a_diff = DNSRecord.parse(diff)
-
-    if args.short:
-        print(a.short())
-    else:
-        print(";; Got answer:")
-        if args.hex:
-            print(";; RESPONSE:",binascii.hexlify(a_pkt).decode())
-            if args.diff and not args.dig:
-                print(";; DIFF    :",binascii.hexlify(diff).decode())
-        print(a)
-        print()
+        if a.header.tc and args.noretry == False:
+            # Truncated - retry in TCP mode
+            a_pkt = q.send(address,port,tcp=True)
+            a = DNSRecord.parse(a_pkt)
 
         if args.dig or args.diff:
-            if q != q_diff:
-                print(";;; ERROR: Diff Question differs")
-                for (d1,d2) in q.diff(q_diff):
-                    if d1:
-                        print(";; - %s" % d1)
-                    if d2:
-                        print(";; + %s" % d2)
-            if a != a_diff:
-                print(";;; ERROR: Diff Response differs")
-                for (d1,d2) in a.diff(a_diff):
-                    if d1:
-                        print(";; - %s" % d1)
-                    if d2:
-                        print(";; + %s" % d2)
+            if args.diff:
+                address,_,port = args.diff.partition(':')
+                port = int(port or 53)
 
-    if args.debug:
-        code.interact(local=locals())
+            if args.dig:
+                if args.dnssec:
+                    dig = getoutput("dig +qr +dnssec -p %d %s %s @%s" % (
+                                    port, args.domain, args.qtype, address))
+                else:
+                    dig = getoutput("dig +qr +noedns +noadflag -p %d %s %s @%s" % (
+                                    port, args.domain, args.qtype, address))
+                dig_reply = list(iter(DigParser(dig)))
+                # DiG might have retried in TCP mode so get last q/a
+                q_diff = dig_reply[-2]
+                a_diff = dig_reply[-1]
+            else:
+                q_diff = DNSRecord(header=DNSHeader(id=q.header.id),
+                                   q=DNSQuestion(args.domain,
+                                                 getattr(QTYPE,args.qtype)))
+                q_diff = q
+                diff = q_diff.send(address,port,tcp=args.tcp)
+                a_diff = DNSRecord.parse(diff)
+                if a_diff.header.tc and args.noretry == False:
+                    diff = q_diff.send(address,port,tcp=True)
+                    a_diff = DNSRecord.parse(diff)
+
+        if args.short:
+            print(a.short())
+        else:
+            print(";; Got answer:")
+            if args.hex:
+                print(";; RESPONSE:",binascii.hexlify(a_pkt).decode())
+                if args.diff and not args.dig:
+                    print(";; DIFF    :",binascii.hexlify(diff).decode())
+            print(a)
+            print()
+
+            if args.dig or args.diff:
+                if q != q_diff:
+                    print(";;; ERROR: Diff Question differs")
+                    for (d1,d2) in q.diff(q_diff):
+                        if d1:
+                            print(";; - %s" % d1)
+                        if d2:
+                            print(";; + %s" % d2)
+                if a != a_diff:
+                    print(";;; ERROR: Diff Response differs")
+                    for (d1,d2) in a.diff(a_diff):
+                        if d1:
+                            print(";; - %s" % d1)
+                        if d2:
+                            print(";; + %s" % d2)
+
+        if args.debug:
+            code.interact(local=locals())
+
+    except DNSError as e:
+        p.error(e)
 
